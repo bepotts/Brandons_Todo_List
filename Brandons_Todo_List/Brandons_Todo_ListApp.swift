297442afb58d109736340eb94ec9f60756b6b634
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-11-05 15:53:52 -0500

    Initial Commit

---
+++ Brandons_Todo_List/Brandons_Todo_ListApp.swift
@@ -0,0 +1,32 @@
+//
+//  Brandons_Todo_ListApp.swift
+//  Brandons_Todo_List
+//
+//  Created by Brandon Potts on 11/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Brandons_Todo_ListApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Brandons_Todo_ListTests/Brandons_Todo_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  Brandons_Todo_ListTests.swift
+//  Brandons_Todo_ListTests
+//
+//  Created by Brandon Potts on 11/5/25.
+//
+
+import Testing
+@testable import Brandons_Todo_List
+
+struct Brandons_Todo_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
