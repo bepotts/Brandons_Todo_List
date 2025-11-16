690ea18dbe2417dcff15231eb77f45f85f4548ba
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-11-16 11:55:30 -0500

    added landpage logo. Doesn't work. Added List to Notes page

--- Brandons_Todo_List/Views/NotesPage.swift
+++ Brandons_Todo_List/Views/NotesPage.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct NotesPage: View {
+    @State private var currentText = ""
+    @State private var noteItems: [NoteText] = []
+    
     var body: some View {
-        Text("This is the Notes Page")
+        List(noteItems, id: \.self) { note in
+            Text(note.text)
+        }
+        TextField("Insert a note", text: $currentText)
+            .textFieldStyle(.roundedBorder)
+            .padding()
+            .onSubmit {
+                print("Submit button pressed")
+                noteItems.append(NoteText(text: currentText))
+            }
+    }
+    
+    func addNote(text: String) {
+        noteItems.append(NoteText(text: text))
     }
 }
 
 #Preview {
     NotesPage()
+}
+
+fileprivate struct NoteText: Identifiable, Hashable {
+    let id: UUID = UUID()
+    var text: String
 }
