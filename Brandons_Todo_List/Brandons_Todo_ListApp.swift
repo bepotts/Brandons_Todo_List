d18af69588452cb1d6e22afadf44f08acbc142a9
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-11-28 11:22:20 -0500

    began to add secrets to the project

--- Brandons_Todo_List/Brandons_Todo_ListApp.swift
+++ Brandons_Todo_List/Brandons_Todo_ListApp.swift
@@ -5,24 +5,36 @@
 //  Created by Brandon Potts on 11/5/25.
 //
 
+import FirebaseCore
 import SwiftUI
 import SwiftData
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
 struct Brandons_Todo_ListApp: App {
+    // used to integerate third party applications
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             NoteText.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
+        
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
